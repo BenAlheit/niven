308c7482ce9d26f4efaa12671451e68e70bb556e
Ben Alheit <alheitb@gmail.com>
2024-12-25 16:41:33 +0100

    Did some cmake packaging stuff to build as a library

---
+++ examples/example2/example2.cpp
@@ -0,0 +1,38 @@
+#include "Timer.h"
+#include <cstdint>
+#include <ctime>
+#include <iostream>
+
+void nested_function() {
+  NivenTimeFunction;
+  for (unsigned int i = 0; i < 10000; i++) {
+    double a = 2 * 2;
+  }
+}
+
+void some_function() {
+  NivenTimeFunction;
+  for (unsigned int i = 0; i < 10000; i++) {
+    double a = 2 * 2;
+    nested_function();
+  }
+}
+
+int main() {
+
+  timer.start();
+  {
+    NivenTimeBlock("outside loop");
+    for (unsigned int i = 0; i < 10000; i++) {
+      NivenTimeBlock("loop");
+
+      double a = 2 * 2;
+    }
+  }
+  some_function();
+  timer.end();
+  timer.print();
+  timer.dump_json("test.json");
+
+  return 0;
+}
